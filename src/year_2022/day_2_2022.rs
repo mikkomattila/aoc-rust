73422936cefce83f77e62b802d2f79545206adee
Mikko Mattila <9086012+mikkomattila@users.noreply.github.com>
2024-11-25 18:44:48 +0200

    Clean 2022-2

--- src/year_2022/day_2_2022.rs
+++ src/year_2022/day_2_2022.rs
@@ -10,6 +10,14 @@ use crate::helpers::fetch_input;
  */
 pub struct Day2;
 
+impl DayResult for Day2 {
+    fn print_day_result() {
+        let input = fetch_input(2, 2022);
+        println!("Result 1: {}", get_result_1(&input));
+        println!("Result 2: {}", get_result_2(&input));
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum Rps {
     Rock,
@@ -24,14 +32,6 @@ enum RpsResult {
     Win,
 }
 
-impl DayResult for Day2 {
-    fn print_day_result() {
-        let input = fetch_input(2, 2022);
-        println!("Result 1: {}", get_result_1(&input));
-        println!("Result 2: {}", get_result_2(&input));
-    }
-}
-
 fn get_result_1(input: &[String]) -> u32 {
     parse_rps_games(input)
         .iter()
@@ -42,30 +42,30 @@ fn get_result_1(input: &[String]) -> u32 {
 }
 
 fn get_result_2(input: &[String]) -> u32 {
-    let mut result: Vec<u32> = Vec::new();
-    let games = parse_rps_games(input);
-
     let round_end_map = get_round_end_map();
     let rps_wins_map = get_rps_wins_map();
     let rps_loses_map = get_rps_loses_map();
 
-    for (player_1, player_2) in games {
-        let end = round_end_map
-            .get(&player_2)
-            .expect("Player 2 not found in round_end_map");
-        let p2_actual = match end {
-            RpsResult::Win => *rps_loses_map
-                .get(&player_1)
-                .expect("Player 1 not found in rps_loses_map"),
-            RpsResult::Draw => player_1,
-            RpsResult::Lose => *rps_wins_map
-                .get(&player_1)
-                .expect("Player 1 not found in rps_wins_map"),
-        };
-        result.push(get_result_score(player_1, p2_actual) + get_rps_score(p2_actual));
-    }
+    parse_rps_games(input)
+        .iter()
+        .map(|(player_1, player_2)| {
+            let round_end_result = round_end_map
+                .get(player_2)
+                .expect("Player 2 not found in round_end_map");
 
-    result.iter().sum()
+            let player_2_round_end = match round_end_result {
+                RpsResult::Win => *rps_loses_map
+                    .get(player_1)
+                    .expect("Player 1 not found in rps_loses_map"),
+                RpsResult::Draw => *player_1,
+                RpsResult::Lose => *rps_wins_map
+                    .get(player_1)
+                    .expect("Player 1 not found in rps_wins_map"),
+            };
+
+            get_result_score(*player_1, player_2_round_end) + get_rps_score(player_2_round_end)
+        })
+        .sum()
 }
 
 fn get_round_end_map() -> HashMap<Rps, RpsResult> {
@@ -121,10 +121,10 @@ fn parse_rps_games(input: &[String]) -> Vec<(Rps, Rps)> {
     result
 }
 
-fn get_result_score(p1: Rps, p2: Rps) -> u32 {
-    match p1 {
-        _ if p1 == p2 => 3,
-        _ if get_rps_wins_map().get(&p2) == Some(&p1) => 6,
+fn get_result_score(player_1: Rps, player_2: Rps) -> u32 {
+    match player_1 {
+        _ if player_1 == player_2 => 3,
+        _ if get_rps_wins_map().get(&player_2) == Some(&player_1) => 6,
         _ => 0,
     }
 }
