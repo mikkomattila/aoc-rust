2ebd4b6dd2e35b701aafd706fac82094f3343aff
Mikko Mattila <9086012+mikkomattila@users.noreply.github.com>
2024-12-02 20:19:05 +0200

    Solve 2024-2 pt1, fix file links in readme.

--- src/year_2024/day_2.rs
+++ src/year_2024/day_2.rs
@@ -3,7 +3,7 @@ use crate::helpers::fetch_input;
 
 /**
  * Day 2 of 2022:
- *
+ * Red-Nosed Reports
  */
 pub struct Day2_2024;
 
@@ -15,8 +15,49 @@ impl DayResult for Day2_2024 {
     }
 }
 
+fn parse_reports(input: &[String]) -> Vec<Vec<u32>> {
+    input
+        .iter()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|s| s.parse::<u32>().expect("Failed to parse string to u32"))
+                .collect()
+        })
+        .collect()
+}
+
+fn is_safe(a: &u32, b: &u32) -> bool {
+    a.abs_diff(*b) >= 1 && a.abs_diff(*b) <= 3
+}
+
 fn get_result_1(input: &[String]) -> u32 {
-    0
+    let mut score = 0;
+    let reports = parse_reports(input);
+
+    for report in reports {
+        let mut invalid = true;
+        let mut asc = false;
+        let mut desc = false;
+
+        for window in report.windows(2) {
+            if let [current, next] = window {
+                if next > current && is_safe(next, current) {
+                    asc = true;
+                } else if next < current && is_safe(next, current) {
+                    desc = true;
+                } else {
+                    invalid = false;
+                    break;
+                }
+            }
+        }
+
+        if invalid && !(asc && desc) {
+            score += 1;
+        }
+    }
+
+    score
 }
 
 fn get_result_2(input: &[String]) -> u32 {
@@ -27,7 +68,14 @@ fn get_result_2(input: &[String]) -> u32 {
 mod tests {
     use super::*;
 
-    static TEST_INPUT: &[&str] = &[];
+    static TEST_INPUT: &[&str] = &[
+        "7 6 4 2 1",
+        "1 2 7 8 9",
+        "9 7 6 2 1",
+        "1 3 2 4 5",
+        "8 6 4 4 1",
+        "1 3 6 7 9",
+    ];
 
     fn get_test_input() -> Vec<String> {
         TEST_INPUT.iter().map(|&s| s.to_string()).collect()
@@ -36,7 +84,7 @@ mod tests {
     #[test]
     fn test_get_result_1() {
         let result = get_result_1(&get_test_input());
-        assert_eq!(result, 0);
+        assert_eq!(result, 2);
     }
 
     #[test]
