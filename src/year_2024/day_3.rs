c00f4327ac8a1fb83f92cf3e3252bef78cf222da
Mikko Mattila <9086012+mikkomattila@users.noreply.github.com>
2024-12-03 22:41:53 +0200

    Solve 2024-3 pt2.

--- src/year_2024/day_3.rs
+++ src/year_2024/day_3.rs
@@ -12,11 +12,12 @@ pub struct Day3_2024;
 impl DayResult for Day3_2024 {
     fn print_day_result() {
         let input = fetch_input(3, 2024).join("\n");
-        println!("Result 1: {}", get_multiplication_result(input));
+        println!("Result 1: {}", get_result_1(input.clone()));
+        println!("Result 2: {}", get_result_2(input));
     }
 }
 
-fn get_multiplication_result(input: String) -> i32 {
+fn get_result_1(input: String) -> i32 {
     Regex::new(r"mul\((\d+),(\d+)\)")
         .expect("Failed to create regex")
         .captures_iter(&input)
@@ -28,16 +29,48 @@ fn get_multiplication_result(input: String) -> i32 {
         .sum()
 }
 
+fn get_result_2(input: String) -> i32 {
+    let mut enabled = true;
+    Regex::new(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))")
+        .expect("Failed to create regex")
+        .captures_iter(&input)
+        .map(|captures| {
+            if captures.get(4).is_some() {
+                enabled = false;
+                0
+            } else if captures.get(3).is_some() {
+                enabled = true;
+                0
+            } else if enabled {
+                let a: i32 = captures[1].parse().expect("Failed to parse string to i32");
+                let b: i32 = captures[2].parse().expect("Failed to parse string to i32");
+                a * b
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    static TEST_INPUT: &str =
+    static TEST_INPUT_1: &str =
         "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
+    static TEST_INPUT_2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
     #[test]
-    fn test_get_multiplication_result() {
-        let result = get_multiplication_result(TEST_INPUT.to_string());
+    fn test_get_result_1() {
+        let result = get_result_1(TEST_INPUT_1.to_string());
         assert_eq!(result, 161);
+    }
+
+    #[test]
+    fn test_get_result_2() {
+        let result = get_result_2(TEST_INPUT_2.to_string());
+        assert_eq!(result, 48);
     }
 }
