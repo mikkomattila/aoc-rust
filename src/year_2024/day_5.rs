7372a36ed444f4f9ee663daadb20a8e2a5ddc8e4
Mikko Mattila <9086012+mikkomattila@users.noreply.github.com>
2024-12-06 01:04:54 +0200

    2024-5: Solve pt2

--- src/year_2024/day_5.rs
+++ src/year_2024/day_5.rs
@@ -10,7 +10,8 @@ pub struct Day5_2024;
 impl DayResult for Day5_2024 {
     fn print_day_result() {
         let input = fetch_input(5, 2024);
-        println!("Result 1: {}", get_result_1(input));
+        println!("Result 1: {}", get_result_1(input.clone()));
+        println!("Result 2: {}", get_result_2(input));
     }
 }
 
@@ -53,9 +54,8 @@ fn parse_input(input: Vec<String>) -> (Vec<(i32, i32)>, Vec<Vec<i32>>) {
 }
 
 fn get_result_1(input: Vec<String>) -> i32 {
-    let (page_ordering_rules, updates) = parse_input(input);
-
     let mut sum = 0;
+    let (page_ordering_rules, updates) = parse_input(input);
 
     for page_sequence in updates {
         let mut is_valid_sequence = true;
@@ -85,9 +85,62 @@ fn get_result_1(input: Vec<String>) -> i32 {
     sum
 }
 
-#[allow(unused)]
 fn get_result_2(input: Vec<String>) -> i32 {
-    0
+    let mut sum = 0;
+    let (page_ordering_rules, updates) = parse_input(input);
+    let mut invalid_page_sequences: Vec<Vec<i32>> = Vec::new();
+
+    let get_allowed_pages = |page_number: i32| -> Vec<i32> {
+        page_ordering_rules
+            .iter()
+            .filter(|&&(s, _)| s == page_number)
+            .map(|&(_, b)| b)
+            .collect::<Vec<i32>>()
+    };
+
+    for page_sequence in updates {
+        for (i, &page_number) in page_sequence.iter().enumerate() {
+            let remaining_pages = &page_sequence[i + 1..];
+            let passed_pages = &page_sequence[..i];
+            let allowed_pages = get_allowed_pages(page_number);
+
+            if !remaining_pages.iter().all(|&r| allowed_pages.contains(&r))
+                || !passed_pages.iter().all(|&r| !allowed_pages.contains(&r))
+            {
+                invalid_page_sequences.push(page_sequence.clone());
+                break;
+            }
+        }
+    }
+
+    for mut sequence in invalid_page_sequences {
+        let fixed_sequence: Vec<i32> = fix_invalid_sequence(&mut sequence[..], &get_allowed_pages);
+        sum += fixed_sequence[fixed_sequence.len() / 2];
+    }
+
+    sum
+}
+
+fn fix_invalid_sequence(
+    invalid_page_sequence: &mut [i32],
+    get_allowed_pages: &dyn Fn(i32) -> Vec<i32>,
+) -> Vec<i32> {
+    for (i, &page_number) in invalid_page_sequence.iter().enumerate() {
+        let passed_pages = &invalid_page_sequence[..i];
+        let allowed_pages = get_allowed_pages(page_number);
+
+        if !passed_pages.iter().all(|&r| !allowed_pages.contains(&r)) {
+            for (j, &passed_page) in passed_pages.iter().enumerate() {
+                if allowed_pages.contains(&passed_page) {
+                    invalid_page_sequence[i] = passed_page;
+                    invalid_page_sequence[j] = page_number;
+                    return fix_invalid_sequence(invalid_page_sequence, get_allowed_pages);
+                }
+            }
+            return fix_invalid_sequence(invalid_page_sequence, get_allowed_pages);
+        }
+    }
+    invalid_page_sequence.to_vec()
 }
 
 #[cfg(test)]
@@ -119,9 +172,9 @@ mod tests {
         "",
         "75,47,61,53,29",
         "97,61,53,29,13",
-        "75,29,13",
-        "75,97,47,61,53",
-        "61,13,29",
+        // "75,29,13",
+        // "75,97,47,61,53",
+        // "61,13,29",
         "97,13,75,29,47",
     ];
 
@@ -135,10 +188,9 @@ mod tests {
         assert_eq!(result, 143);
     }
 
-    #[ignore = "TODO"]
     #[test]
     fn test_get_result_2() {
         let result = get_result_2(get_test_input());
-        assert_eq!(result, 0);
+        assert_eq!(result, 123);
     }
 }
